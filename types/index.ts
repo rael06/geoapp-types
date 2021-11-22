4938dfec1b623f1905627bb8555034f92893cbed
Rael Calitro <rael06@hotmail.fr>
2021-11-22 19:54:02 +0100

    add config

---
+++ types/index.ts
@@ -0,0 +1,37 @@
+export type SearchResponse = {
+  type: string;
+  version: string;
+  features: Feature[];
+  attribution: string;
+  licence: string;
+  query: string;
+  limit: number;
+};
+
+export type Feature = {
+  type: string;
+  geometry: Geometry;
+  properties: Properties;
+};
+
+export type Geometry = {
+  type: string;
+  coordinates: number[];
+};
+
+export type Properties = {
+  label: string;
+  score: number;
+  id: string;
+  name: string;
+  postcode: string;
+  citycode: string;
+  x: number;
+  y: number;
+  city: string;
+  context: string;
+  type: string;
+  importance: number;
+  housenumber: string;
+  street: string;
+};
